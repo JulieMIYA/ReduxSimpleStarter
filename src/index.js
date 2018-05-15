a2e603fcafce65f869f98a24bb6613ff13ae8fa1
bingbing <bingbing.yan@yellowkorner.fr>
2018-05-15 18:07:59 +0200

    define onselect event

--- src/index.js
+++ src/index.js
@@ -11,27 +11,32 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 const API_KEY ="AIzaSyDGtI5lkjhgddynWBCna19yc1xqx1CAsg0";
-YTSearch({key: API_KEY, term: 'health food'}, function(data){
-  console.log(data);
-});
+
 class App extends Component{
   constructor(props){
         super(props);
-        this.state = {videos : [] };
+        this.state = {
+          videos : [] ,
+          selectedVideo : null
+        };
 
         YTSearch({key: API_KEY, term: 'health food'}, (videos)=>{
           // this.setState({videos: videos});
-          this.setState({videos});
+          this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+          });
         });
   }
+
   render(){
     return (
       <div>
         <div className="row">
           <SearchBar />
         </div>
-        <VideoDetail video={this.state.videos[0]}/> 
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/> 
+        <VideoList videos={this.state.videos} onVideoSelect={selectedVideo =>this.setState({selectedVideo}) } />
       </div>
     );
   }
